e272dcc8d24647926430a5ccadb0d975451d43ba
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2022-12-19 16:56:36 -0800

    implemented & tested recursive ParseTreeNode.evaluate() method

--- ParseTreeNode.java
+++ ParseTreeNode.java
@@ -1,97 +1,102 @@
 package org.magentatobe.jcalculator;
 
+import static java.lang.Float.NaN;
+import java.util.StringJoiner;
+
 public class ParseTreeNode {
 
-    private String leftChildStr;
-    private ParseTreeNode leftChildNode;
-    private Character centerOperator;
-    private ParseTreeNode rightChildNode;
-    private String rightChildStr;
+    private final String leftChildStr;
+    private final ParseTreeNode leftChildNode;
+    private final Character centerOperator;
+    private final ParseTreeNode rightChildNode;
+    private final String rightChildStr;
 
     public ParseTreeNode(final Character centerOperatorVal, final ParseTreeNode soleChildNodeVal) {
-        if (centerOperator == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
-        if (soleChildNode == null)  { throw new IllegalArgumentException("argument 'soleChildNodeVal' was null"); }
+        if (centerOperatorVal == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
+        if (soleChildNodeVal == null)  { throw new IllegalArgumentException("argument 'soleChildNodeVal' was null"); }
         leftChildStr = null;
         leftChildNode = null;
         centerOperator = centerOperatorVal;
         rightChildNode = soleChildNodeVal;
         rightChildStr = null;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
     }
 
     public ParseTreeNode(final Character soleOperatorVal, final String soleChildStrVal) {
-        if (soleOperator == null) { throw new IllegalArgumentException("argument 'soleOperatorVal' was null"); }
-        if (soleChildStr == null) { throw new IllegalArgumentException("argument 'soleChildStrVal' was null"); }
+        if (soleOperatorVal == null) { throw new IllegalArgumentException("argument 'soleOperatorVal' was null"); }
+        if (soleChildStrVal == null) { throw new IllegalArgumentException("argument 'soleChildStrVal' was null"); }
         leftChildStr = null;
         leftChildNode = null;
         centerOperator = soleOperatorVal;
         rightChildNode = null;
         rightChildStr = soleChildStrVal;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
     }
 
     public ParseTreeNode(final String leftChildStrVal, final Character centerOperatorVal,
-                         final ParseTreeNode rightChildNode) {
-        if (leftChildStr == null)   { throw new IllegalArgumentException("argument 'leftChildStrVal' was null"); }
-        if (centerOperator == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
-        if (rightChildNode == null) { throw new IllegalArgumentException("argument 'rightChildNodeVal' was null"); }
-        leftChildStr = leftChildStr;
+                         final ParseTreeNode rightChildNodeVal) {
+        if (leftChildStrVal == null)   { throw new IllegalArgumentException("argument 'leftChildStrVal' was null"); }
+        if (centerOperatorVal == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
+        if (rightChildNodeVal == null) { throw new IllegalArgumentException("argument 'rightChildNodeVal' was null"); }
+        leftChildStr = leftChildStrVal;
         leftChildNode = null;
-        centerOperator = centerOperator;
-        rightChildNode = rightChildNode;
+        centerOperator = centerOperatorVal;
+        rightChildNode = rightChildNodeVal;
         rightChildStr = null;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
     }
 
     public ParseTreeNode(final ParseTreeNode leftChildNodeVal, final Character centerOperatorVal,
-                         final ParseTreeNode rightChildNode) {
-        if (leftChildNode == null)  { throw new IllegalArgumentException("argument 'leftChildNodeVal' was null"); }
-        if (centerOperator == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
-        if (rightChildNode == null) { throw new IllegalArgumentException("argument 'rightChildNodeVal' was null"); }
+                         final ParseTreeNode rightChildNodeVal) {
+        if (leftChildNodeVal == null)  { throw new IllegalArgumentException("argument 'leftChildNodeVal' was null"); }
+        if (centerOperatorVal == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
+        if (rightChildNodeVal == null) { throw new IllegalArgumentException("argument 'rightChildNodeVal' was null"); }
         leftChildStr = null;
-        leftChildNode = leftChildNode;
-        centerOperator = centerOperator;
-        rightChildNode = rightChildNode;
+        leftChildNode = leftChildNodeVal;
+        centerOperator = centerOperatorVal;
+        rightChildNode = rightChildNodeVal;
         rightChildStr = null;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
     }
 
-    public ParseTreeNode(final String leftChildStrVal, final Character centerOperatorVal, final String rightChildStr) {
-        if (leftChildStr == null)   { throw new IllegalArgumentException("argument 'leftChildStrVal' was null"); }
-        if (centerOperator == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
-        if (rightChildStr == null)  { throw new IllegalArgumentException("argument 'rightChildStrVal' was null"); }
-        leftChildStr = leftChildStr;
+    public ParseTreeNode(final String leftChildStrVal, final Character centerOperatorVal, final String rightChildStrVal) {
+        if (leftChildStrVal == null)   { throw new IllegalArgumentException("argument 'leftChildStrVal' was null"); }
+        if (centerOperatorVal == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
+        if (rightChildStrVal == null)  { throw new IllegalArgumentException("argument 'rightChildStrVal' was null"); }
+        leftChildStr = leftChildStrVal;
         leftChildNode = null;
-        centerOperator = centerOperator;
+        centerOperator = centerOperatorVal;
         rightChildNode = null;
-        rightChildStr = rightChildStr;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
+        rightChildStr = rightChildStrVal;
     }
 
     public ParseTreeNode(final ParseTreeNode leftChildNodeVal, final Character centerOperatorVal,
-                         final String rightChildStr) {
-        if (leftChildNode == null)  { throw new IllegalArgumentException("argument 'leftChildNodeVal' was null"); }
-        if (centerOperator == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
-        if (rightChildStr == null)  { throw new IllegalArgumentException("argument 'rightChildStrVal' was null"); }
+                         final String rightChildStrVal) {
+        if (leftChildNodeVal == null)  { throw new IllegalArgumentException("argument 'leftChildNodeVal' was null"); }
+        if (centerOperatorVal == null) { throw new IllegalArgumentException("argument 'centerOperatorVal' was null"); }
+        if (rightChildStrVal == null)  { throw new IllegalArgumentException("argument 'rightChildStrVal' was null"); }
         leftChildStr = null;
-        leftChildNode = leftChildNode;
-        centerOperator = centerOperator;
+        leftChildNode = leftChildNodeVal;
+        centerOperator = centerOperatorVal;
         rightChildNode = null;
-        rightChildStr = rightChildStr;
-        assert !(leftChildStr != null && leftChildNode != null) && !(rightChildNode != null && rightChildStr != null);
+        rightChildStr = rightChildStrVal;
     }
 
-    public String getLeftChildStr()          { return leftChildStr; }
-    public ParseTreeNode getLeftChildNode()  { return leftChildNode; }
-    public Character getCenterOperator()     { return centerOperator; }
-    public ParseTreeNode getRightChildNode() { return rightChildNode; }
-    public String getRightChildStr()         { return rightChildStr; }
-
-    public void setLeftChildStr(String leftChildStrVal)            { leftChildStr = leftChildStrVal; }
-    public void setLeftChildNode(ParseTreeNode leftChildNodeVal)   { leftChildNode = leftChildNodeVal; }
-    public void setCenterOperator(Character centerOperatorVal)     { centerOperator = centerOperatorVal; }
-    public void setRightChildNode(ParseTreeNode rightChildNodeVal) { rightChildNode = rightChildNodeVal; }
-    public void setRightChildStr(String rightChildStrVal)          { rightChildStr = rightChildStrVal; }
+    public float evaluate() throws NullPointerException {
+        Float leftChildValue = (leftChildStr != null) ? Float.valueOf(leftChildStr) : (leftChildNode != null) ? leftChildNode.evaluate() : null;
+        Float rightChildValue = (rightChildStr != null) ? Float.parseFloat(rightChildStr) : rightChildNode.evaluate();
+        switch (centerOperator) {
+            case '+':
+                return leftChildValue + rightChildValue;
+            case '−':
+                return (leftChildValue != null) ? leftChildValue - rightChildValue : -rightChildValue;
+            case '×':
+                return leftChildValue * rightChildValue;
+            case '÷':
+                return leftChildValue / rightChildValue;
+            case '^':
+                return (float) Math.pow(leftChildValue, rightChildValue);
+            case '√':
+                return (float) Math.sqrt(rightChildValue);
+        }
+        return NaN;
+    }
 
     public String toString() {
         StringJoiner strJoin = new StringJoiner(", ", "{ ", " }");
--- junit/TestArithmeticParser.java
+++ junit/TestArithmeticParser.java
@@ -3,37 +3,41 @@ package org.magentatobe.jcalculator.junit;
 import junit.framework.TestCase;
 
 import org.magentatobe.jcalculator.ArithmeticParser;
-
+import org.magentatobe.jcalculator.ParseTreeNode;
 
 public final class TestArithmeticParser extends TestCase {
 
     public void testParseExpression1() {
         ArithmeticParser arithmeticParser = new ArithmeticParser("1+1");
-        ArithmeticParser.ParseTreeNode parseTree = arithmeticParser.parseExpression();
-        String parseTreeStr = parseTree.recursiveToString();
+        ParseTreeNode parseTree = arithmeticParser.parseExpression();
+        String parseTreeStr = parseTree.toString();
         assertEquals("{ { 1 }, '+', { 1 } }", parseTreeStr);
+        assertEquals(2F, parseTree.evaluate());
     }
 
     public void testParseExpression2() {
         ArithmeticParser arithmeticParser = new ArithmeticParser("1+2+3+(4+5)");
-        ArithmeticParser.ParseTreeNode parseTree = arithmeticParser.parseExpression();
-        String parseTreeStr = parseTree.recursiveToString();
+        ParseTreeNode parseTree = arithmeticParser.parseExpression();
+        String parseTreeStr = parseTree.toString();
         assertEquals("{ { { { 1 }, '+', { 2 } }, '+', { 3 } }, '+', { { 4 }, '+', { 5 } } }", parseTreeStr);
+        assertEquals(15F, parseTree.evaluate());
     }
 
     public void testParseExpression3() {
         ArithmeticParser arithmeticParser = new ArithmeticParser("1+2×3+(4×5+6)");
-        ArithmeticParser.ParseTreeNode parseTree = arithmeticParser.parseExpression();
-        String parseTreeStr = parseTree.recursiveToString();
+        ParseTreeNode parseTree = arithmeticParser.parseExpression();
+        String parseTreeStr = parseTree.toString();
         assertEquals("{ { { 1 }, '+', { { 2 }, '×', { 3 } } }, '+', { { { 4 }, '×', { 5 } }, '+', { 6 } } }",
                      parseTreeStr);
+        assertEquals(33F, parseTree.evaluate());
     }
 
     public void testParseExpression4() {
-        ArithmeticParser arithmeticParser = new ArithmeticParser("−1+2×3+(4×5−√6)+7^8^9");
-        ArithmeticParser.ParseTreeNode parseTree = arithmeticParser.parseExpression();
-        String parseTreeStr = parseTree.recursiveToString();
-        assertEquals("{ { { { '−', { 1 } }, '+', { { 2 }, '×', { 3 } } }, '+', { { { 4 }, '×', { 5 } }, '−', "
-                     + "{ '√', { 6 } } } }, '+', { { 7 }, '^', { { 8 }, '^', { 9 } } } }", parseTreeStr);
+        ArithmeticParser arithmeticParser = new ArithmeticParser("−1+2×3+(4×5−√4)+2^2^2");
+        ParseTreeNode parseTree = arithmeticParser.parseExpression();
+        String parseTreeStr = parseTree.toString();
+        assertEquals("{ { { { '−', { 1 } }, '+', { { 2 }, '×', { 3 } } }, '+', { { { 4 }, '×', { 5 } }, '−', { '√', "
+                     + "{ 4 } } } }, '+', { { 2 }, '^', { { 2 }, '^', { 2 } } } }", parseTreeStr);
+        assertEquals(39F, parseTree.evaluate());
     }
 }
